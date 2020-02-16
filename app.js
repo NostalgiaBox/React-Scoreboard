73bcd505cf4a66b4a31822d161168bfe800e98a7
NostalgiaBox <rfenster@gmail.com>
2020-02-15 20:24:54 -0500

    Added placeholder text

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const desc = "I just learned how to create a React Node!"
-const myTitleID = 'main-title';
-const name = 'Murray'
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
-const header = (
-    <header>
-    {/* This is a comment */}
-        <h1 id={ myTitleID }>{ name }'s First React Element</h1>
-        <p className="main-desc">{ desc }</p>
-    </header>
-);
 ReactDOM.render(
     header,
     document.getElementById('root')
