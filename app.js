84b209df7871192e5b5d25c4b42b4f6139fdf68c
NostalgiaBox <rfenster@gmail.com>
2020-02-15 20:33:15 -0500

    Added placeholder Player component

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ function Header() {
     );
 }
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Murray
+            </span>
+
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">35</span>
+                <button className="counter-action increment"> + </button>
+            </div>
+        </div>
+    );
+}
 ReactDOM.render(
-    header,
+    <Player />,
     document.getElementById('root')
 );
