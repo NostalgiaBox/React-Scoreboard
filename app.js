328780ab222b79c524b5110662e81e6cd218155c
NostalgiaBox <rfenster@gmail.com>
2020-02-15 21:35:44 -0500

    Added unique id to each player

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const players = [
     {
         name: "Guil",
-        score: 50
+        score: 50,
+        id: 1
       },
       {
         name: "Treasure",
-        score: 85
+        score: 85,
+        id: 2
       },
       {
         name: "Ashley",
-        score: 95
+        score: 95,
+        id: 3
       },
       {
         name: "James",
-        score: 80
+        score: 80,
+        id: 4
       }
 ];
 
@@ -60,7 +64,9 @@ const App = (props) => {
 
             {/* Player List */}
             {props.initialPlayers.map( player =>
-                <Player playerName={player.name} playerScore={player.score} />
+                <Player playerName={player.name} playerScore={player.score} 
+                    key={player.id.toString()}
+                />
 
             )}
         </div>
